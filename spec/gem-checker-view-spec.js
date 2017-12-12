7524ccf8b52ac43e9b66354719f7bf06ddb616fd
Jamie <up733026@myport.ac.uk>
2017-12-12 02:01:53 +0000

    phase 1 gem version checker

---
+++ spec/gem-checker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GemCheckerView from '../lib/gem-checker-view';
+
+describe('GemCheckerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
