c3588824b3e36bf68fe2b9b761f522dad470b3bc
Jamie <up733026@myport.ac.uk>
2018-01-16 19:43:22 +0000

    gem details spec

--- spec/gem-checker-spec.js
+++ spec/gem-checker-spec.js
@@ -1,6 +1,8 @@
 'use babel';
 
 import GemChecker from '../lib/gem-checker';
+import $ from "jquery";
+const GemDetective = require('../lib/gem_detective');
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -24,16 +26,26 @@ describe('GemChecker', () => {
       // This is an activation event, triggering it will cause the package to be
       // activated.
       editor = atom.workspace.getActiveTextEditor();
-      editor.setCursorBufferPosition([2, 8]);
+      editor.setCursorBufferPosition([3, 8]);
+      spyOn( $, 'ajax' ).andCallFake( function (params) { 
+        params.success({name: 'devise', info: "Flexible authentication solution for Rails with Warden",
+                        gem_uri: "https://rubygems.org/gems/devise-4.4.0.gem",
+                        homepage_uri: "https://github.com/plataformatec/devise",
+                        project_uri: "https://rubygems.org/gems/devise",
+                        version: "4.4.0"
+                       }, {}, {status: 200}); 
+      });
       atom.commands.dispatch(workspaceElement, 'gem-checker:get_gem_details');
-
       waitsForPromise(() => {
         return activationPromise;
       });
 
       runs(() => {
-        console.log(atom.notifications.getNotifications());
         expect(atom.notifications.getNotifications().length).toBe(1);
+        notification = atom.notifications.getNotifications()[0]
+        expect(notification['message']).toBe("Gem Information");
+        expect(notification['type']).toBe("info");
+        expect(notification['options']['detail']).toBe("Name: devise\n \nDescription: Flexible authentication solution for Rails with Warden\n \nLatest Version: 4.4.0");
       });
     });
   });
