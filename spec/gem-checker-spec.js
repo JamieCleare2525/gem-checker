e3621a89a753bde2e188aaf3648c928f2905c7fb
Jamie <up733026@myport.ac.uk>
2018-01-16 23:23:26 +0000

    correct spec

--- spec/gem-checker-spec.js
+++ spec/gem-checker-spec.js
@@ -20,8 +20,8 @@ describe('GemChecker', () => {
     activationPromise = atom.packages.activatePackage('gem-checker');
   });
 
-  describe('when the gem-detective:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
+  describe('when the gem-checker:get_gem_details event is triggered on a gem', () => {
+    it('shows a info notification containing the details of the gem', () => {
 
       // This is an activation event, triggering it will cause the package to be
       // activated.
@@ -46,6 +46,34 @@ describe('GemChecker', () => {
         expect(notification['message']).toBe("Gem Information");
         expect(notification['type']).toBe("info");
         expect(notification['options']['detail']).toBe("Name: devise\n \nDescription: Flexible authentication solution for Rails with Warden\n \nLatest Version: 4.4.0");
+      });
+    });
+  });
+
+  describe('when the gem-checker:check_gem_maintenance event is triggered on a gem', () => {
+    it('shows a warning notification containing the version details of the gem', () => {
+      editor = atom.workspace.getActiveTextEditor();
+      editor.setCursorBufferPosition([3, 8]);
+      spyOn( $, 'ajax' ).andCallFake( function (params) { 
+        params.success([{name: 'devise', info: "Flexible authentication solution for Rails with Warden",
+                        gem_uri: "https://rubygems.org/gems/devise-4.4.0.gem",
+                        built_at: "2015-03-14T00:00:00.000Z",
+                        homepage_uri: "https://github.com/plataformatec/devise",
+                        project_uri: "https://rubygems.org/gems/devise",
+                        number: "4.4.0"
+                       }], {}, {status: 200}); 
+      });
+      atom.commands.dispatch(workspaceElement, 'gem-checker:check_gem_maintenance');
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(atom.notifications.getNotifications().length).toBe(1);
+        notification = atom.notifications.getNotifications()[0]
+        expect(notification['message']).toBe("Warning: Gem Not Maintained");
+        expect(notification['type']).toBe("warning");
+        expect(notification['options']['detail']).toBe("This Gem has not been updated since Sat Mar 14 2015 00:00:00 GMT+0000 (GMT), this may indicate that the Gem is no longer maintained.");
       });
     });
   });
