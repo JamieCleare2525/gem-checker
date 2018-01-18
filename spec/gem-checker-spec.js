e50e8ff1ac18c2c4874bf1ea5bbb10c9b36eba8d
Jamie <up733026@myport.ac.uk>
2018-01-18 18:12:18 +0000

    improve performance using Detective Class

--- spec/gem-checker-spec.js
+++ spec/gem-checker-spec.js
@@ -20,7 +20,7 @@ describe('GemChecker', () => {
     activationPromise = atom.packages.activatePackage('gem-checker');
   });
 
-  describe('when the gem-checker:get_gem_details event is triggered on a gem', () => {
+  describe('when the gem-checker:gem_info event is triggered on a gem', () => {
     it('shows a info notification containing the details of the gem', () => {
 
       // This is an activation event, triggering it will cause the package to be
@@ -35,7 +35,7 @@ describe('GemChecker', () => {
                         version: "4.4.0"
                        }, {}, {status: 200}); 
       });
-      atom.commands.dispatch(workspaceElement, 'gem-checker:get_gem_details');
+      atom.commands.dispatch(workspaceElement, 'gem-checker:gem_info');
       waitsForPromise(() => {
         return activationPromise;
       });
@@ -45,7 +45,7 @@ describe('GemChecker', () => {
         notification = atom.notifications.getNotifications()[0]
         expect(notification['message']).toBe("Gem Information");
         expect(notification['type']).toBe("info");
-        expect(notification['options']['detail']).toBe("Name: devise\n \nDescription: Flexible authentication solution for Rails with Warden\n \nLatest Version: 4.4.0");
+        expect(notification['options']['detail']).toBe("Name: devise\n \nDescription: Flexible authentication solution for Rails with Warden\n \nCurrent Version: 4.3.0\nLatest Version: 4.4.0");
       });
     });
   });
@@ -55,13 +55,22 @@ describe('GemChecker', () => {
       editor = atom.workspace.getActiveTextEditor();
       editor.setCursorBufferPosition([3, 8]);
       spyOn( $, 'ajax' ).andCallFake( function (params) { 
-        params.success([{name: 'devise', info: "Flexible authentication solution for Rails with Warden",
-                        gem_uri: "https://rubygems.org/gems/devise-4.4.0.gem",
-                        built_at: "2015-03-14T00:00:00.000Z",
-                        homepage_uri: "https://github.com/plataformatec/devise",
-                        project_uri: "https://rubygems.org/gems/devise",
-                        number: "4.4.0"
-                       }], {}, {status: 200}); 
+        if (params['url'] == "https://rubygems.org/api/v1/versions/devise.json"){
+          params.success([{name: 'devise', info: "Flexible authentication solution for Rails with Warden",
+                          gem_uri: "https://rubygems.org/gems/devise-4.4.0.gem",
+                          built_at: "2015-03-14T00:00:00.000Z",
+                          homepage_uri: "https://github.com/plataformatec/devise",
+                          project_uri: "https://rubygems.org/gems/devise",
+                          number: "4.4.0"
+                         }], {}, {status: 200}); 
+        }else{
+          params.success({name: 'devise', info: "Flexible authentication solution for Rails with Warden",
+                          gem_uri: "https://rubygems.org/gems/devise-4.4.0.gem",
+                          homepage_uri: "https://github.com/plataformatec/devise",
+                          project_uri: "https://rubygems.org/gems/devise",
+                          version: "4.4.0"
+                         }, {}, {status: 200}); 
+        }
       });
       atom.commands.dispatch(workspaceElement, 'gem-checker:check_gem_maintenance');
       waitsForPromise(() => {
@@ -73,7 +82,7 @@ describe('GemChecker', () => {
         notification = atom.notifications.getNotifications()[0]
         expect(notification['message']).toBe("Warning: Gem Not Maintained");
         expect(notification['type']).toBe("warning");
-        expect(notification['options']['detail']).toBe("This Gem has not been updated since Sat Mar 14 2015 00:00:00 GMT+0000 (GMT), this may indicate that the Gem is no longer maintained.");
+        expect(notification['options']['detail']).toBe("Name: devise\n \nThis Gem has not been updated since:\nSat Mar 14 2015 00:00:00 GMT+0000 (GMT)\n \nThis may indicate that the Gem is no longer maintained.");
       });
     });
   });
